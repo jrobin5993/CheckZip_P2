691be58f12daffccbc8c64c9c7192f5f6ad577d3
jrobin5993 <jrobin5993@my.sullivan.edu>
2024-06-25 10:26:00 -0400

    [Display] From CheckZip Part I

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/*
+ * Name:            JaLeel Robinson
+ * Date:            06-26-2024
+ * Descriptiopn:    CSC242-0101-CheckZip
+ * Resource:
+ */
+
+namespace CheckZip
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            // This program allows the user to enter 10 zip codes.
+            // The program then sorts the data and displays the
+            // data in order.
+
+            // Initialize the array with 10 zip codes
+            int[] zipCodes = new int[10];
+            Console.WriteLine("Please enter 10 zip codes, press Enter after each.");
+
+            // Loop to collect zip codes from the user
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter zip code [i + 1]: ");
+                while (!int.TryParse(Console.ReadLine(), out zipCodes[i]) || zipCodes[i] < 0)
+                {
+                    Console.Write("Invalid input. Please enter a valid positive zip code: ");
+                }
+            }
+
+            //Sort the zip codes
+            Array.Sort(zipCodes);
+            Console.WriteLine("\nThe sorted zip codes are:");
+
+            // Loop to display the sorted zip codes
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(zipCodes[i]);
+            }
+
+            // Keep the console window open
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
